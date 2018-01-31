8837480d8e953de453cf6887ead23e5376c508b2
Jonathan Haigh <jonathan.haigh@arm.com>
2018-01-31 10:46:27 +0000

    IOTMBL-140: rpi3 image doesn't boot (kernel panic)
    The partition scheme implemented on RPi3 by
    mbed_sdcard_image-rpi.bbclass didn't match what the U-Boot script
    expected.
    
    Getting things right with mbed_sdcard_image-rpi.bbclass is much harder
    than just specifying the partition layout in a wks file and allowing WIC
    to do the rest, so switch to using WIC for our RPi3 SD card images and
    update the U-boot script to match.
    
    The wks file added for RPi3 is the same as the wks file for Warp7 except
    that there's no partition for a raw U-boot image and the boot partition
    is a little bit bigger.
    
    Add some comments to the RPi3 U-boot script because the U-boot commands
    can be a little cryptic.
    
    Also resolves:
    IOTMBL-76: See if we can use WIC for creating rpi3 images
    
    Signed-off-by: Jonathan Haigh <jonathan.haigh@arm.com>

--- recipes-bsp/mbl-boot-scr/files/raspberrypi3/boot.cmd
+++ recipes-bsp/mbl-boot-scr/files/raspberrypi3/boot.cmd
@@ -1,6 +1,17 @@
+# Load device tree and get kernel args from it
 fdt addr ${fdt_addr} && fdt get value bootargs /chosen bootargs
-setenv rootfs /dev/mmcblk0p5
-ext4size mmc 0:2 rootfs2 && setenv rootfs /dev/mmcblk0p6
+
+# mmcblk0p3 is rootfs1 - use this by default
+setenv rootfs /dev/mmcblk0p3
+
+# mmcblk0p5 is rootfs2 - use this if there's a "rootfs2" flag in the flags partition
+ext4size mmc 0:2 rootfs2 && setenv rootfs /dev/mmcblk0p5
+
+echo "using root=${rootfs}"
 setenv bootargs "${bootargs} root=${rootfs}"
+
+# Load Linux from the boot partition
 fatload mmc 0:1 ${kernel_addr_r} uImage
+
+# Boot Linux with the device tree
 bootm ${kernel_addr_r} - ${fdt_addr}

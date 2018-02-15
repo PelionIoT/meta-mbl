313dfff4a8e8fec502df4299b0916459e49bebb6
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-02-15 17:13:39 +0000

    mbl-boot-src: imx7s-warp: Enable secure boot path in boot.scr
    This patch enables secure-boot based on MBL work based on the current state
    of u-boot for WaRP7 in the Linaro branch - ported over the ARM's yocto
    layer.
    
    The boot script here works both for secure and non-secure boot paths
    locating uTee.optee, zImage and FDT based on the fs-layout produced by
    ARM's main MBL build.
    
    When the secure boot bit in OTP fuses is enabled the environment variable
    hab_enabled = 1, when secure boot is not enabled hab_enabled = 0. In order
    to prevent an attacker from subverting hab_enabled a real deployment would
    set the environment variable stdin to nil.
    
    Selecting slots is as simple as doing "setenv mmcpart part-number".
    
    The load order is
    
    - U-boot loads boot.scr or boot.scr-signed depending on hab_enabled
    - Boot.scr loads a kernel or a signed kernel depending on hab_enabled
    - Boot.scr loads fdt or a fdt signed depending on hab_enabled
    - Boot.scr switches from loading from FAT to loading from one of the slots
    - Boot.scr loads uTee.optee or uTee.optee signed depending on hab_enabled
    - Boot.scr determines the correct UUID for the root filesystem
    - Boot.scr chain-loads to OPTEE
    
    Whenever hab_enabled is true any of the signed images - including u-boot,
    boot.scr, zImage, FDT and OPTEE must pass verification via a callback into
    the i.MX7 BootROM.
    
    If calling into the BootROM HAB fails at any stage then the i.MX7 will drop
    into USB recovery mode.
    
    - BootROM hab_enabled -> Load u-boot -> automatically verifies internally
      Pass - continue to next step
      Fail - automatically sets device into USB recovery mode
    
    - U-boot hab_enabled -> Loads boot.scr-signed -> verifies via callback
      Pass - executes contents of boot script
      Fail - makes callback into BootROM to put device into USB recovery mode
    
    - Boot.src hab_enabled -> Loads zImage.imx-signed -> verifies via callback
      Pass - continues executing contents of boot script
      Fail - makes callback into BootROM to put device into USB recovery mode
    
    - Boot.src hab_enabled -> Loads fdt.imx-signed -> verifies via callback
      Pass - continues executing contents of boot script
      Fail - makes callback into BootROM to put device into USB recovery mode
    
    - Boot.src hab_enabled -> Loads uTee.optee.imx-signed -> verifies via callback
      Pass - continues executing contents of boot script
      Fail - makes callback into BootROM to put device into USB recovery mode
    
    - Chain-loads into Optee
      Optee subsequently loads Linux kernel from known memory location
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- recipes-bsp/mbl-boot-scr/files/imx7s-warp-mbl/boot.cmd
+++ recipes-bsp/mbl-boot-scr/files/imx7s-warp-mbl/boot.cmd
@@ -1,5 +1,46 @@
-setenv root /dev/mmcblk1p3
-setenv mmcextpart 3
-ext4size mmc 0:2 rootfs2 && setenv root /dev/mmcblk1p5 && setenv mmcextpart 5
-setenv mmcargs setenv bootargs console=${console},${baudrate} root=${root} rootwait rw
-run loadimage && run mmcbootsec
+# This section is responsbile for loading a signed Linux kernel
+setenv image_signed zImage.imx-signed
+if test ${hab_enabled} -eq 1; then
+	hab_get_ivt_addr ${loadaddr} ${ivt_offset}
+	${loadcmd} mmc ${mmcdev}:${mmcpart} ${hab_ivt_addr} ${image_signed}
+	run warp7_auth_or_fail
+else
+	run loadimage;
+fi
+
+# This section is responsbile for loading a signed FDT image
+setenv fdt_file_signed imx7s-warp.dtb.imx-signed
+if test ${hab_enabled} -eq 1; then
+	hab_get_ivt_addr ${fdt_addr} ${ivt_offset}
+	${loadcmd} mmc ${mmcdev}:${mmcpart} ${hab_ivt_addr} ${fdt_file_signed}
+	run warp7_auth_or_fail
+else
+	run loadfdt;
+fi
+
+# Set the filesystem type and partition target
+setenv loadcmd ext4load
+setenv mmcpart 5
+
+# This section is responsbile for loading a signed OPTEE image
+setenv optee_file /lib/firmware/uTee.optee
+setenv optee_file_signed /lib/firmware/uTee.optee.imx-signed
+setenv loadoptee "${loadcmd} mmc ${mmcdev}:${mmcpart} ${optee_addr} ${optee_file}"
+if test ${hab_enabled} -eq 1; then
+	hab_get_ivt_addr ${optee_addr} ${ivt_offset}
+	${loadcmd} mmc ${mmcdev}:${mmcpart} ${hab_ivt_addr} ${optee_file_signed}
+	run warp7_auth_or_fail
+else
+	run loadoptee;
+fi
+
+# Set UUID mmcpart will be used to pass root id to kernel
+run finduuid;
+run mmcargs;
+
+# Now boot
+echo Booting secure Linux/OPTEE OS from mmc ...;
+bootm ${optee_addr} - ${fdt_addr};
+
+# Failsafe if something goes wrong
+hab_failsafe

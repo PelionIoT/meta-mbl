ef9e052feacf4a0957d1e140425ac6e52d354ba0
Rui Miguel Silva <rui.silva@linaro.org>
2018-06-29 18:04:56 +0100

    mbl-boot-scr: remove dtb and optee entries
    With the use of ARM Trusted Firmware boot flow the DTB and optee are now handle
    and validated by the FIP image and because of that not loaded by the u-boot as
    BL33.
    So, remove the entries related to DTB and optee from the boot script and move
    the bootm command from loading optee to load the linux kernel.
    
    Signed-off-by: Rui Miguel Silva <rui.silva@linaro.org>

--- recipes-bsp/mbl-boot-scr/files/imx7s-warp-mbl/boot.cmd
+++ recipes-bsp/mbl-boot-scr/files/imx7s-warp-mbl/boot.cmd
@@ -11,16 +11,6 @@ else
 	run loadimage;
 fi
 
-# This section is responsbile for loading a signed FDT image
-setenv fdt_file_signed imx7s-warp.dtb.imx-signed
-if test ${hab_enabled} -eq 1; then
-	setexpr hab_ivt_addr ${fdt_addr} - ${ivt_offset}
-	load mmc ${mmcdev}:${mmcpart} ${hab_ivt_addr} ${fdt_file_signed}
-	run warp7_auth_or_fail
-else
-	run loadfdt;
-fi
-
 # In order to load OP-TEE Boot from rootfs1 by default
 setenv mmcpart 3
 
@@ -32,26 +22,14 @@ ext4size mmc 0:2 rootfs2 && setenv mmcpart 5
 # The "rootfs=" part of the kernel command line still exists but it is ignored
 # as the initramfs has an init script.
 
-# This section is responsbile for loading a signed OPTEE image
-setenv optee_file /lib/firmware/uTee.optee
-setenv optee_file_signed /lib/firmware/uTee.optee.imx-signed
-setenv loadoptee "load mmc ${mmcdev}:${mmcpart} ${optee_addr} ${optee_file}"
-if test ${hab_enabled} -eq 1; then
-	setexpr hab_ivt_addr ${optee_addr} - ${ivt_offset}
-	load mmc ${mmcdev}:${mmcpart} ${hab_ivt_addr} ${optee_file_signed}
-	run warp7_auth_or_fail
-else
-	run loadoptee;
-fi
-
 # Set UUID mmcpart will be used to pass root id to kernel
 setenv rootpart ${mmcpart}
 run finduuid;
 run mmcargs;
 
 # Now boot
-echo Booting secure Linux/OPTEE OS from mmc ...;
-bootm ${optee_addr} - ${fdt_addr};
+echo Booting secure Linux from mmc ...;
+bootm ${loadaddr} - ${fdt_addr};
 
 # Failsafe if something goes wrong
 hab_failsafe

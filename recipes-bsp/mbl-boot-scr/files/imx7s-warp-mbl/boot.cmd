624a4e507d1363ce4fc9d28b3c9d38bcd4b3bb24
Rui Miguel Silva <rui.silva@linaro.org>
2018-06-29 18:04:56 +0100

    mbl-boot-scr: remove dtb and optee entries
    With the use of ARM Trusted Firmware boot flow the DTB and optee are now handle
    and validated by the FIP image and because of that not loaded by the u-boot as
    BL33.
    So, remove the entries related to DTB and optee from the boot script and move
    the bootm command from loading optee to load the linux kernel.
    
    Signed-off-by: Rui Miguel Silva <rui.silva@linaro.org>

--- recipes-bsp/mbl-boot-scr/files/imx7s-warp-mbl/boot.cmd
+++ recipes-bsp/mbl-boot-scr/files/imx7s-warp-mbl/boot.cmd
@@ -8,33 +8,11 @@ else
 	run loadimage;
 fi
 
-# This section is responsbile for loading a signed FDT image
-setenv fdt_file_signed imx7s-warp.dtb.imx-signed
-if test ${hab_enabled} -eq 1; then
-	setexpr hab_ivt_addr ${fdt_addr} - ${ivt_offset}
-	load mmc ${mmcdev}:${mmcpart} ${hab_ivt_addr} ${fdt_file_signed}
-	run warp7_auth_or_fail
-else
-	run loadfdt;
-fi
-
 # Boot from rootfs1 by default
 setenv mmcpart 3
 
 # But if the rootfs2 file exists in partition 2, boot from rootfs2
 ext4size mmc 0:2 rootfs2 && setenv mmcpart 5
-
-# This section is responsbile for loading a signed OPTEE image
-setenv optee_file /lib/firmware/uTee.optee
-setenv optee_file_signed /lib/firmware/uTee.optee.imx-signed
-setenv loadoptee "load mmc ${mmcdev}:${mmcpart} ${optee_addr} ${optee_file}"
-if test ${hab_enabled} -eq 1; then
-	setexpr hab_ivt_addr ${optee_addr} - ${ivt_offset}
-	load mmc ${mmcdev}:${mmcpart} ${hab_ivt_addr} ${optee_file_signed}
-	run warp7_auth_or_fail
-else
-	run loadoptee;
-fi
 
 # Set UUID mmcpart will be used to pass root id to kernel
 setenv rootpart ${mmcpart}
@@ -42,8 +20,8 @@ run finduuid;
 run mmcargs;
 
 # Now boot
-echo Booting secure Linux/OPTEE OS from mmc ...;
-bootm ${optee_addr} - ${fdt_addr};
+echo Booting secure Linux from mmc ...;
+bootm ${loadaddr} - ${fdt_addr};
 
 # Failsafe if something goes wrong
 hab_failsafe

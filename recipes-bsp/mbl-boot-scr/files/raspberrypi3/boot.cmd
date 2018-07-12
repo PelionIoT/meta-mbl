b1b1cb96e644750bcc944cb53e0533f3bdfb0c93
Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
2018-07-12 14:56:25 +0800

    rpi3: add FIT image support
    This commit enables the usage of U-boot FIT image. Thus the kernel and
    dtb and bootscripts are packaged into FIT and can be verify by u-boot.
    
    Migrates to using RPi3B+ instead of RPi3B.
    
    We need to put FIT image into FAT partition. Previously it is 20MB. And
    filled with VideoCore4 boot binarnies. We enlarge it to 40MB.
    
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paulliu@debian.org>

--- recipes-bsp/mbl-boot-scr/files/raspberrypi3/boot.cmd
+++ recipes-bsp/mbl-boot-scr/files/raspberrypi3/boot.cmd
@@ -1,8 +1,6 @@
 # Load device tree and get kernel args from it
 setenv fdt_addr 0x03000000
 
-fdt addr ${fdt_addr} && fdt get value bootargs /chosen bootargs
-
 # mmcblk0p3 is rootfs1 - use this by default
 setenv rootfs /dev/mmcblk0p3
 
@@ -10,10 +8,8 @@ setenv rootfs /dev/mmcblk0p3
 ext4size mmc 0:2 rootfs2 && setenv rootfs /dev/mmcblk0p5
 
 echo "using root=${rootfs}"
-setenv bootargs "${bootargs} root=${rootfs} memmap=16M$256M dwc_otg.fiq_enable=0 dwc_otg.fiq_fsm_enable=0 dwc_otg.nak_holdoff=0"
-
-# Load Linux from the boot partition
-fatload mmc 0:1 ${kernel_addr_r} uImage
+setenv bootargs "${bootargs} 8250.nr_uarts=1 bcm2708_fb.fbwidth=656 bcm2708_fb.fbheight=416 bcm2708_fb.fbswap=1 vc_mem.mem_base=0x3ec00000 vc_mem.mem_size=0x40000000 dwc_otg.lpm_enable=0 rootfstype=ext4 console=ttyS0,115200 rootwait root=${rootfs} memmap=16M$256M dwc_otg.fiq_enable=0 dwc_otg.fiq_fsm_enable=0 dwc_otg.nak_holdoff=0"
 
 # Boot Linux with the device tree
-bootm ${kernel_addr_r} - ${fdt_addr}
+load mmc 0 0x02100000 kernel.itb
+bootm 0x02100000
--- recipes-bsp/u-boot/u-boot_%.bbappend
+++ recipes-bsp/u-boot/u-boot_%.bbappend
@@ -4,4 +4,13 @@ RDEPENDS_${PN}_remove = "rpi-u-boot-scr"
 RDEPENDS_${PN}_append = " mbl-boot-scr"
 
 FILESEXTRAPATHS_prepend := "${THISDIR}/u-boot:"
-SRC_URI_append_rpi = " file://0100-rpi3-Change-u-boot-loading-address.patch "
+SRC_URI_append_rpi = " file://0100-rpi3-Change-u-boot-loading-address.patch \
+		   file://rpi3-boot-script-fit-file.diff \
+		   "
+
+do_deploy_append_rpi() {
+
+       install -d ${DEPLOYDIR}/fip
+       install -m 0644 ${B}/dts/dt.dtb ${DEPLOYDIR}/fip/dt.dtb
+       install -m 0644 ${B}/u-boot-nodtb.bin ${DEPLOYDIR}/fip/u-boot-nodtb.bin
+}

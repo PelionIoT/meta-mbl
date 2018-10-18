ac6db2a2f5fcc9350106ee8881724629c0234e88
Bryan O'Donoghue <pure.logic@nexus-software.ie>
2018-10-18 17:56:53 +0100

    mbl-boot-src: imx7s-warp: Remove rootfs selection data (#237)
    Previous versions of this file selected rootfs by looking for the presence
    of a magic file. More recent changes in the initramfs make the rootfs
    selection happen inside the initramfs as opposed to in the bootscript.
    
    Remove the bootscript rootfs selection logic now.

--- recipes-bsp/mbl-boot-scr/files/imx7s-warp-mbl/boot.cmd
+++ recipes-bsp/mbl-boot-scr/files/imx7s-warp-mbl/boot.cmd
@@ -1,22 +1,9 @@
-# In order to load OP-TEE Boot from rootfs1 by default
-setenv mmcpart 3
-
-# But if the rootfs2 file exists in partition 2, boot from rootfs2
-ext4size mmc 0:2 rootfs2 && setenv mmcpart 5
-
-# Note: rootfs partition is selected in initramfs-init-script.sh and depends on 
-# the existence of rootfs file in the bootflags partition.
-# The "rootfs=" part of the kernel command line still exists but it is ignored
-# as the initramfs has an init script.
-
-# Set UUID mmcpart will be used to pass root id to kernel
-setenv rootpart ${mmcpart}
-run finduuid;
-run mmcargs;
+# The rootfs selection is done in the initramfs other kernel params are set in mmcargs
+run mmcargs
 
 # Now boot
 echo Booting secure Linux from FIT ...;
-bootm ${bootscriptaddr}#conf@1 - ${fdt_addr};
+bootm ${bootscriptaddr}#conf@1 ${bootscriptaddr}:ramdisk@1 ${fdt_addr}
 
 # Failsafe if something goes wrong
 hab_failsafe

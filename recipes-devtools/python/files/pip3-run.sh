5f805381dc2738be9ebe235dd29b0741118885d2
Hugues Kamba <hugues.kamba@arm.com>
2018-11-26 10:20:09 +0000

    python3: Remove additional files used to set up the obsolete test environment (#289)
    For IOTMBL-936 Remove recipes for packages that are installed in the
    test environment

--- recipes-devtools/python/files/pip3-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Copyright (c) 2018 Arm Limited and Contributors. All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-
-source __REPLACE_ME_WITH_MBL_APP_DIR__/__REPLACE_ME_WITH_libdir__/set-up-test-env.sh
-exec pip3.5 "$@"

ca447b27f0551f35bb6d4f73f6c22198f0a49564
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2019-03-14 13:11:38 +0000

    qca-9377-firmware-tools: Add target side scripts to install QCA firmware
    For licensing reasons we do not ship the QCA-9377 firmware. This recipe
    installs a script on the target which will instead allow post-bootup
    installation of the necessary firmware.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

---
+++ recipes-kernel/kernel-modules/kernel-module-qca9377-firmware-tools/populate_rootfs_qca.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD 3-Clause
+
+FW=firmware-qca-2.0.3
+FWBIN=$FW.bin
+
+if [ ! -f $FWBIN ]; then
+	wget https://www.nxp.com/lgfiles/NMG/MAD/YOCTO/$FWBIN
+	rm -rf $FW
+fi
+
+if [ ! -f $FW ]; then
+	chmod +x $FWBIN
+	./$FWBIN
+fi
+
+cp -v -r ./$FW/1PJ_QCA9377-3_LEA_2.0/* /

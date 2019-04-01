623aab274ffa3dbff1efd94ffa418e28603bb2ea
Diego Sueiro <diego.sueiro@arm.com>
2019-04-01 13:58:26 +0100

    mbl-usb-gether-mac-addr: Persist usb gether MAC addr across updates
    We have to persist the usb gether MAC addr across reboots and the
    current implementation relies on saving these information in
    /etc/modprobe.d/g_ether.conf . This path doesn't persist after an
    update and we can't symlink it to /config/user/modprobe.d/g_ether.conf
    because in the early stages of the boot process (before running
    systemd-modules-load.service) the /config is not mounted.
    
    To overcome this restriction we are removing the g_ether from the
    KERNEL_MODULE_AUTOLOAD and loading the g_ether module with the MAC addr
    in the mbl-usb-gether-mac-addr.service.
    
    Fixes #470

---
+++ recipes-core/systemd/systemd-net-conf-dbg-iface/mbl-usb-gether-mac-addr.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+if [ ! -s __REPLACE_ME_WITH_MBL_NON_FACTORY_CONFIG_DIR__/modprobe.d/g_ether.conf ]; then
+    mkdir -p __REPLACE_ME_WITH_MBL_NON_FACTORY_CONFIG_DIR__/modprobe.d
+    # In the first octet of the MAC addr we need to ensure that b0 is 0 (unicast)
+    # and b1 is 1 (locally administered).
+    # We decided to prefix the dev_addr with "ee" and host_addr with "aa".
+    dev_addr=ee:$(cut -b 0-8,10-11 /proc/sys/kernel/random/uuid | sed "s/../&:/g;s/:$//")
+    host_addr=aa:$(cut -b 0-8,10-11 /proc/sys/kernel/random/uuid | sed "s/../&:/g;s/:$//")
+    echo "options g_ether dev_addr=$dev_addr host_addr=$host_addr" > __REPLACE_ME_WITH_MBL_NON_FACTORY_CONFIG_DIR__/modprobe.d/g_ether.conf
+fi
+
+modprobe -C __REPLACE_ME_WITH_MBL_NON_FACTORY_CONFIG_DIR__/modprobe.d/g_ether.conf g_ether

0a2fa553490c56b00bca3c9d5faac17cfb8d9efc
Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
2019-05-17 18:02:35 +0800

    IOTMBL-2022: imx8mmevk: Convert kernel image type to FIT (#548)
    * imx8mmevk: Convert kernel image type to FIT
    
    IOTMBL-2022: i.MX8 FIT refactor: change u-boot config and header to support
    fit image and boot from it
    
    pack kernel, initramfs and device tree in FIT image format.
    
    Signed-off-by: Jun Nie <jun.nie@linaro.org>
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paul.liu@linaro.org>

--- meta-freescale-mbl/recipes-bsp/mbl-boot-scr/files/imx8mmevk-mbl/boot.cmd
+++ meta-freescale-mbl/recipes-bsp/mbl-boot-scr/files/imx8mmevk-mbl/boot.cmd
@@ -2,7 +2,20 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+# Change root partition to /dev/mmcblk1p3
 setenv mmcroot /dev/mmcblk1p3 rootwait rw
-setenv image Image-initramfs-imx8mmevk-mbl.bin
-run loadimage
-run mmcboot
+
+# Set bootscriptaddr
+setenv bootscriptaddr 0x55000000
+
+# The rootfs selection is done in the initramfs other kernel params are set in mmcargs
+run mmcargs
+
+echo "Load fit blob with Linux Kernel image and initramfs image"
+fatload mmc 0 ${bootscriptaddr} boot.scr
+
+# Extract FDT from FIT
+imxtract ${bootscriptaddr}#conf@freescale_fsl-imx8mm-evk.dtb fdt@freescale_fsl-imx8mm-evk.dtb ${fdt_addr}
+
+# Boot Linux
+bootm ${bootscriptaddr}#conf@freescale_fsl-imx8mm-evk.dtb ${bootscriptaddr}:ramdisk@1 ${fdt_addr}
--- meta-freescale-mbl/recipes-bsp/u-boot/u-boot-imx_%.bbappend
+++ meta-freescale-mbl/recipes-bsp/u-boot/u-boot-imx_%.bbappend
@@ -3,10 +3,18 @@
 # SPDX-License-Identifier: MIT
 
 SRCBRANCH = "imx_v2018.03_4.14.78_1.0.0_ga-mbl"
-SRC_URI = "git://git.linaro.org/landing-teams/working/mbl/u-boot.git;protocol=https;nobranch=1"
+SRC_URI = "git://git.linaro.org/landing-teams/working/mbl/u-boot.git;protocol=https;nobranch=1 \
+	   file://0001-arm-imx-Add-mbl-specific-boot-option.patch \
+"
+
 SRCREV = "336522718c23e6adb1fe206fc0beab4465d5ecda"
 
 FILESEXTRAPATHS_prepend := "${THISDIR}/u-boot-imx:"
+
+do_compile_append_imx8mmevk-mbl() {
+	# Link device tree to default name for fit image signature verification usage.
+	ln -sf ${config}/dts/dt.dtb ${B}/${UBOOT_DTB_BINARY}
+}
 
 do_deploy_append() {
     # Fixup the name expected by the incoming imx-boot recipe

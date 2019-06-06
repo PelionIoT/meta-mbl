eb577c8d472ceb8e3d648380e41c71975857c6f7
Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
2019-06-06 15:49:21 +0800

    imx8mm optee master overlay (#570)
    * imx8: use the base of optee-os as same as other platform
    
    IOTMBL-1143: Rebase to OPTEE 3.3.x that supports DTB overlay
    
    This commit changes to use the optee-os from the upstream master branch.
    
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
    
    * imx8: Enable DT overlay
    
    IOTMBL-2057: i.MX8 OPTEE overlay support
    
    A patch submitted to the upstream not get accepted yet. This
    patch is necessary for the overlay support for OPTEE. We cherrypick
    that commit and apply it ourselves.
    
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paulliu@debian.org>

--- meta-freescale-mbl/recipes-bsp/mbl-boot-scr/files/imx8mmevk-mbl/boot.cmd
+++ meta-freescale-mbl/recipes-bsp/mbl-boot-scr/files/imx8mmevk-mbl/boot.cmd
@@ -8,6 +8,9 @@ setenv mmcroot /dev/mmcblk1p3 rootwait rw
 # Set bootscriptaddr
 setenv bootscriptaddr 0x55000000
 
+# Set fdtovaddr
+setenv fdtovaddr 0x44000000
+
 # The rootfs selection is done in the initramfs other kernel params are set in mmcargs
 run mmcargs
 
@@ -16,6 +19,11 @@ fatload mmc 0 ${bootscriptaddr} boot.scr
 
 # Extract FDT from FIT
 imxtract ${bootscriptaddr}#conf@freescale_fsl-imx8mm-evk.dtb fdt@freescale_fsl-imx8mm-evk.dtb ${fdt_addr}
+
+# Apply OP-TEE provided overlay
+fdt addr ${fdt_addr}
+fdt resize 0x1000
+fdt apply ${fdtovaddr}
 
 # Boot Linux
 bootm ${bootscriptaddr}#conf@freescale_fsl-imx8mm-evk.dtb ${bootscriptaddr}:ramdisk@1 ${fdt_addr}

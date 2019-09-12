ca83f2e44e237e30e142852745fab8f81fe18560
Diego Sueiro <diego.sueiro@arm.com>
2019-09-12 13:03:49 +0100

    meta-mbl-distro: Replace "Non Factory Config" with just "Config"
    We decided that "config" is good enough to represent the configurations
    that are not related to the factory configurations.
    
    This patch replaces all NON_FACTORY_CONFIG (and its variations) with
    CONFIG (and its variations).

--- meta-mbl-distro/recipes-core/systemd/systemd-net-conf-dbg-iface/mbl-usb-gether-mac-addr.sh
+++ meta-mbl-distro/recipes-core/systemd/systemd-net-conf-dbg-iface/mbl-usb-gether-mac-addr.sh
@@ -3,14 +3,14 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-if [ ! -s __REPLACE_ME_WITH_MBL_NON_FACTORY_CONFIG_DIR__/modprobe.d/g_ether.conf ]; then
-    mkdir -p __REPLACE_ME_WITH_MBL_NON_FACTORY_CONFIG_DIR__/modprobe.d
+if [ ! -s __REPLACE_ME_WITH_MBL_CONFIG_DIR__/modprobe.d/g_ether.conf ]; then
+    mkdir -p __REPLACE_ME_WITH_MBL_CONFIG_DIR__/modprobe.d
     # In the first octet of the MAC addr we need to ensure that b0 is 0 (unicast)
     # and b1 is 1 (locally administered).
     # We decided to prefix the dev_addr with "ee" and host_addr with "aa".
     dev_addr=ee:$(cut -b 0-8,10-11 /proc/sys/kernel/random/uuid | sed "s/../&:/g;s/:$//")
     host_addr=aa:$(cut -b 0-8,10-11 /proc/sys/kernel/random/uuid | sed "s/../&:/g;s/:$//")
-    echo "options g_ether dev_addr=$dev_addr host_addr=$host_addr" > __REPLACE_ME_WITH_MBL_NON_FACTORY_CONFIG_DIR__/modprobe.d/g_ether.conf
+    echo "options g_ether dev_addr=$dev_addr host_addr=$host_addr" > __REPLACE_ME_WITH_MBL_CONFIG_DIR__/modprobe.d/g_ether.conf
 fi
 
-modprobe -C __REPLACE_ME_WITH_MBL_NON_FACTORY_CONFIG_DIR__/modprobe.d/g_ether.conf g_ether
+modprobe -C __REPLACE_ME_WITH_MBL_CONFIG_DIR__/modprobe.d/g_ether.conf g_ether
